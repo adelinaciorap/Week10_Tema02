74d07c3438f6c44f185852a46991b2b07be9b6e5
Adelina Ciorap <c.adelina@outlook.com>
2019-07-05 15:10:56 +0300

    .

---
+++ 01.Strings/01.Strings/StringReverse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Strings
+{
+    public class StringReverse
+    {
+        public string reverseOnlyLetters(string S)
+        {
+            char[] str = S.ToCharArray();
+            int i = 0, j = str.Length - 1;
+            while (i < j)
+            {
+                while (i < j && !char.IsLetter(str[i])) i++;
+                while (i < j && !char.IsLetter(str[j])) j--;
+                swap(str, i++, j--);
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append(str);
+            return sb.ToString();
+        }
+        private void swap(char[] str, int i, int j)
+        {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+        }
+    }
+}
